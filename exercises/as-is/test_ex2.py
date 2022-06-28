3d35673a268a2228e6c67534f47b0bce3440cae0
SATO Shigeyuki <sato.shigeyuki@mi.u-tokyo.ac.jp>
2022-06-29 00:36:06 +0900

    Update as-is exercise examples to ones enabling autograde.

---
+++ exercises/as-is/test_ex2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S testcase_translator.py -sa -cf
+
+square = [
+    ((2,), 2**2),
+    ((1,), 1**2),
+]
---
+++ exercises/as-is/test_ex3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S testcase_translator.py
+
+power = [
+    ((2,3), 2**3),
+    ((1,4), 1**4),
+]
